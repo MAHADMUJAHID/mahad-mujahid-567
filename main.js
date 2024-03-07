fc6da1dfe882d25c3a2f01e368da104f041ce209
MAHADMUJAHID <162475166+MAHADMUJAHID@users.noreply.github.com>
2024-03-07 02:07:57 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,34 @@
-var alienColor = "gray";
-if (alienColor === "black") {
-    console.log("alien color is gray");
+var alienColor = "green";
+if (alienColor == "green") {
+    console.log("version 1 : print a message that the player earned 5 point");
 }
-else {
-    console.log("upeer vale is false");
+else if (alienColor == "yellow") {
+    console.log("print a message that the player earned 10 point");
 }
+else if (alienColor == "red") {
+    console.log(" print a message that the player earned 15 points");
+}
+//vession 2 
+var alienColor2 = "yellow";
+if (alienColor2 == "green") {
+    console.log("print a message that the player earned 5 point");
+}
+else if (alienColor2 == "yellow") {
+    console.log("Version 2 : print a message that the player earned 10 point");
+}
+else if (alienColor2 == "red") {
+    console.log(" print a message that the player earned 15 points");
+}
+//version 3 
+var alienColor3 = "red";
+if (alienColor3 == "green") {
+    console.log("print a message that the player earned 5 point");
+}
+else if (alienColor3 == "yellow") {
+    console.log("print a message that the player earned 10 point");
+}
+else if (alienColor3 == "red") {
+    console.log(" version 3 : print a message that the player earned 15 points");
+}
+// is main alien wali value ko red kr ke answer nikale ga jaise abhi green hai to green ka answer de ga 5 points 
+//or agr yellow main ho ta yellow main dega red ka red main 
